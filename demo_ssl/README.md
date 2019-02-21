8a8786f303d5b0d39c80c023e4343b3d115bc83e
Sundara Senthil SN <C-Sundara.Senthil1@charter.com>
2019-02-20 19:50:28 -0500

    gRPC with ssl

---
+++ demo_ssl/README.md
@@ -0,0 +1,24 @@
+Simple Client/Server model communication using protobuf and Go lang and ssl
+
+Need to create a ssl certificate using this below commands
+$ openssl genrsa -out cert/server.key 2048
+$ openssl req -new -x509 -sha256 -key cert/server.key -out cert/server.crt -days 3650
+$ openssl req -new -sha256 -key cert/server.key -out cert/server.csr
+$ openssl x509 -req -sha256 -in cert/server.csr -signkey cert/server.key -out cert/server.crt -days 3650
+
+
+Using Makefile to build and test
+> make help
+> make dep
+
+Open two terminal and run
+> make build_server
+> ./server.bin
+and in another terminal
+> make build_client
+> ./clent.bin
+
+or after make dep
+we can run server and client, like below too
+> go run server/main.go
+> go run client/main.go
---
+++ demo_ssl/client/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"sample/grpc/demo_ssl/api"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+func main() {
+	var conn *grpc.ClientConn
+
+	// Create the client TLS credentials
+	creds, err := credentials.NewClientTLSFromFile("cert/server.crt", "")
+	if err != nil {
+		log.Fatalf("could not load tls cert: %s", err)
+	}
+
+	conn, err = grpc.Dial("localhost:7777", grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := api.NewPingClient(conn)
+
+	response, err := c.SayHello(context.Background(), &api.PingMessage{Message: "I am Client here"})
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+	log.Printf("Response from server: %s", response.Message)
+}
